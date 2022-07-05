32315b3c6a24200ac298125ce07f54bde9dcbb05
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-05 16:12:44 -0300

    Update DataSource adding database information

--- SimpleChatJava/src/br/com/chat/controller/DataSouce.java
+++ SimpleChatJava/src/br/com/chat/controller/DataSouce.java
@@ -6,8 +6,30 @@ package br.com.chat.controller;
 
 /**
  *
+ * Classe que faz a conexão com o banco de dados MySQL.
+ * 
  * @author Marcos
  */
 public class DataSouce {
+    
+    /*
+    Preencha as variáveis abaixo com os seguintes dados:
+    
+        username - nome do seu usuário MySQL.
+        hostname - nome do host da conexão MySQL onde o banco de dados está.
+        port - porta da conexão MySQL onde o banco de dados está.
+        password - senha da conexão MySQL caso haja.
+        database - nome do seu banco de dados.
+        
+    */
+    
+    String username = "";
+    String password = "";
+    
+    String hostname = "";
+    int port = 000;
+    String database = "";
+    
+    //String url = "jdbc:mysql://"+hostname+":"+port+"/"+database;
     
 }

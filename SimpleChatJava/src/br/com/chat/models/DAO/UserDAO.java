51edf608496beed15d08edcb44eab35694f4369e
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-08 17:31:59 -0300

    Create UserDAO

---
+++ SimpleChatJava/src/br/com/chat/models/DAO/UserDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.chat.models.DAO;
+
+/**
+ *
+ * Classe responsável por manipular os dados dos objetos no banco de dados.
+ * 
+ * @author Marcos
+ */
+public class UserDAO {
+    
+}

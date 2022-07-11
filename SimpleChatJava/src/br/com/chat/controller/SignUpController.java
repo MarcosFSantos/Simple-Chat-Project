9438c90ebce5a10afcf7cdabf1323998b2da14d4
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-11 18:08:23 -0300

    Update SignUpController create checkEmptyField method

--- SimpleChatJava/src/br/com/chat/controller/SignUpController.java
+++ SimpleChatJava/src/br/com/chat/controller/SignUpController.java
@@ -4,6 +4,8 @@
  */
 package br.com.chat.controller;
 
+import javax.swing.JOptionPane;
+
 /**
  * 
  * Classe responsável pela regra de negócio da página de cadastro de usuário.
@@ -14,5 +16,28 @@ public class SignUpController {
     private String username;
     private String password;
     
+    public SignUpController(String username, String password, String confirmPassword){
+        
+        this.username = username;
+        
+        
+        
+    }
+    
+    /**
+     * Esse método checa se os campos dados como parâmetros estâo ou não vazios.
+     * 
+     * @param field
+     * @return boolean value
+     */
+    public boolean checkEmptyField(String field){
+        if (field.equals("")){
+            JOptionPane.showMessageDialog(null, "Fields can not be empty!");
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     
 }

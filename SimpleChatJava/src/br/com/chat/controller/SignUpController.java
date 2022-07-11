a09f757418494c4782bb725f748420d1ce6707a4
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-11 17:38:49 -0300

    Create SignUpController class

---
+++ SimpleChatJava/src/br/com/chat/controller/SignUpController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.chat.controller;
+
+/**
+ * 
+ * Classe responsável pela regra de negócio da página de cadastro de usuário.
+ * 
+ * @author Marcos
+ */
+public class SignUpController {
+    
+}

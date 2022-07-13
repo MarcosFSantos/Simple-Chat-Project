a0b5cc56cb2ddbb5311a3f96084d5d997a335dc6
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-13 16:24:15 -0300

    Create LoginController

---
+++ SimpleChatJava/src/br/com/chat/controller/LoginControler.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.chat.controller;
+
+/**
+ * 
+ * Classe responsável pela regra de negócio da página de login.
+ * 
+ * @author Marcos
+ */
+public class LoginControler {
+    
+}

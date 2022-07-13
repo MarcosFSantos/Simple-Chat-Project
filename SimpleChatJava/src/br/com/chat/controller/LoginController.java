3acc78019f4300e7102fcbb99128fe44ea37a0b9
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-13 16:52:39 -0300

    Update LoginController creating checkEmptyField method

--- SimpleChatJava/src/br/com/chat/controller/LoginController.java
+++ SimpleChatJava/src/br/com/chat/controller/LoginController.java
@@ -4,6 +4,8 @@
  */
 package br.com.chat.controller;
 
+import javax.swing.JOptionPane;
+
 /**
  * 
  * Classe responsável pela regra de negócio da página de login.
@@ -26,5 +28,20 @@ public class LoginController {
         this.password = password;
     }
     
+    /**
+     * Esse método checa se os campos dados como parâmetros estâo ou não vazios.
+     * 
+     * @param field
+     * @return
+     */
+    public boolean checkEmptyField(String field){
+        if (field.equals("")){
+            JOptionPane.showMessageDialog(null, "Fields can not be empty!");
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     
 }

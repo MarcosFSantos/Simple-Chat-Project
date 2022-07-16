adcb3bc8c5295bd449777a30f508ddee2ed86e24
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-07-16 13:02:13 -0300

    Update MessageDAO creating read() method

--- SimpleChatJava/src/br/com/chat/models/DAO/MessageDAO.java
+++ SimpleChatJava/src/br/com/chat/models/DAO/MessageDAO.java
@@ -4,6 +4,14 @@
  */
 package br.com.chat.models.DAO;
 
+import br.com.chat.models.Message;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  * 
  * Classe responsável por manipular os dados dos objetos no banco de dados.
@@ -23,4 +31,43 @@ public class MessageDAO {
         this.dataSource = dataSource;
     }
     
+    /**
+     * Retorna todos as mensagens do banco de dados no formato de uma lista de objetos.
+     * 
+     * @return data
+     */
+    public List<Message> read(){
+        List<Message> data = new ArrayList<>();
+        String SQL = "SELECT * FROM chat.message";
+        
+        try{
+            
+            PreparedStatement ps = dataSource.getConnection().prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {                
+                
+                Message message = new Message();
+                
+                message.setId(rs.getInt("id"));
+                message.setText(rs.getString("text"));
+                message.setTime(rs.getString("time"));
+                message.setUserId(rs.getInt("user_id"));
+                
+                data.add(message);
+            }
+            
+            ps.close();
+            return data;
+            
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "error in retrieve data: "+ex.getMessage());
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "error: "+ex.getMessage());
+        }
+        
+        return null;
+    }
 }
